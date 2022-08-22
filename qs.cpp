a287ce648b29477b4bcc2235fe57f2deb394bc56
Sososhas <111687599+Sososhas@users.noreply.github.com>
2022-08-22 17:23:30 +0300

    тут норм

--- qs.cpp
+++ qs.cpp
@@ -1,39 +1,56 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="ф" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/ф" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/ф" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions />
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+#include <stdlib.h>
+
+float det(float a, float b, float c)
+ {
+  float D;
+   D=b*b-4*a*c;
+  printf("D=%f",D);
+  return D;
+ }
+
+void roots(float a, float b, float c,float D)
+ {
+  float x1,x2;
+   x1=-b+sqrt(D)/2*a;
+   x1=-b-sqrt(D)/2*a;
+  printf("x1=%f\nx2=%f",x1,x2);
+ }
+
+int main()
+{
+  float a,b,c,x,D;
+  printf("enter a,b,c\n");
+   scanf("%f %f %f",&a,&b,&c);
+  printf("a=%f\nb=%f\nc=%f\n",a,b,c);
+
+ if (a==0)
+   {
+    x=-c/b;
+    printf("x=%f",x);
+   }
+   else
+    {
+     D=det(a,b,c);
+        if (D>0)
+         roots(a,b,c,D);
+        if (D=0)
+        {
+          printf("x1=x2");
+          roots(a,b,c,D) ;
+        }
+      else
+         {
+        printf("no real roots\n");
+
+        D=abs(D);
+        roots(a,b,c,D);
+
+      }
+
+
+     }
+
+return 0;
+
+}
